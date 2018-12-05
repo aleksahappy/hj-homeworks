f1db656529b67a07479182fa69b7353658cad63b
Aleksahappy <aleksa.happy@gmail.com>
2018-12-05 23:46:13 +0400

    Доработка/ Способы поиска нужного HTML-элемента/ Задача 2. Ударная установка

--- html-element-collection/drum-machine/js/drum-machine.js
+++ html-element-collection/drum-machine/js/drum-machine.js
@@ -5,6 +5,7 @@ const drumBtns = document.getElementsByClassName('drum-kit__drum');
 function playAudio() {
   const audio = this.getElementsByTagName('audio')[0];
   audio.play();
+  audio.currentTime = 0;
 }
 
 for (const btn of drumBtns) {
