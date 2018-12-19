1981dc4aace4f8543acc27604d575364c876542c
Aleksahappy <aleksa.happy@gmail.com>
2018-12-20 02:02:01 +0400

    Принцип обработки событий/ Задача 3. Добавить в корзину

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+list.addEventListener('click', ckickBtnAddToCard);
+
+function ckickBtnAddToCard(event) {
+  if (!event.target.classList.contains('add-to-cart')){
+    return;
+  }
+  event.preventDefault();
+
+  const item = {
+    title: event.target.dataset.title,
+    price: event.target.dataset.price
+  };
+
+  addToCart(item);
+}
