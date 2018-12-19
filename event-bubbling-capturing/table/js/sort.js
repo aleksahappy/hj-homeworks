e93366e0e6059d5e15cf2fa250fcab2564ba612d
Aleksahappy <aleksa.happy@gmail.com>
2018-12-20 00:55:03 +0400

    Принцип обработки событий/ Задача 2. Сортировка таблицы

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.tagName !== 'TH') {
+    return;
+  }
+
+  if (!event.target.dataset.dir || event.target.dataset.dir === '-1') {
+    event.target.dataset.dir = '1';
+  } else {
+    event.target.dataset.dir = '-1';
+  }
+
+  event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+  sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);
 }
