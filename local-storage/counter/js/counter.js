2007669d32d647488b499f3de29be34698bd6ea2
Aleksahappy <aleksa.happy@gmail.com>
2018-12-24 05:24:03 +0400

    Хранение состояния на клиенте и отправка на сервер/ Задача 1. Персональный счетчик

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const textCounter = document.getElementById('counter');
+const btnIncrement = document.getElementById('increment');
+const btnDecrement = document.getElementById('decrement');
+const btnReset = document.getElementById('reset');
+
+btnIncrement.addEventListener('click', increaseCounter);
+btnDecrement.addEventListener('click', decreaseCounter);
+btnReset.addEventListener('click', resetCounter);
+
+if (!localStorage.counter) {
+  localStorage.counter = '0';
+}
+textCounter.textContent = localStorage.counter;
+
+function increaseCounter() {
+  textCounter.textContent = ++localStorage.counter;
+}
+
+function decreaseCounter() {
+  if (localStorage.counter > 0) {
+    textCounter.textContent = --localStorage.counter;
+  }
+}
+
+function resetCounter() {
+  localStorage.counter = '0';
+  textCounter.textContent = localStorage.counter;
+}
