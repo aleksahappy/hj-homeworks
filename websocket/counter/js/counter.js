0735c37113769972df6067be9b785a9e2861298a
Aleksahappy <aleksa.happy@gmail.com>
2018-12-29 01:50:09 +0400

    Веб-сокеты/ Задача 2. Счетчик открытых соединений

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('output.errors');
+
+connection.addEventListener('message', updateCounter);
+window.addEventListener('beforeunload', closeСonnection);
+
+function updateCounter(event) {
+  try {
+    const data = JSON.parse(event.data);
+    counter.textContent = data.connections;
+    errors.textContent = data.errors;
+  } catch(error) {
+    console.error(error.message);
+  }
+}
+
+function closeСonnection(event) {
+  connection.close(1000);
+}
