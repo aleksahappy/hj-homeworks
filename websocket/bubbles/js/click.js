84e1ed4f5368e203e19f9598efb40760fbe1184b
Aleksahappy <aleksa.happy@gmail.com>
2018-12-29 01:48:33 +0400

    Веб-сокеты/ Задача 1. Шарики

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', () => {
+  showBubbles(connection);
+  window.addEventListener('click', showClick);
+});
+
+function showClick(event) {
+  connection.send(JSON.stringify({x: event.clientX, y: event.clientY}));
+}
