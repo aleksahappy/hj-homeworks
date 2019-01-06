4bde0593454281b464870d726760eeab3711c44e
Aleksahappy <aleksa.happy@gmail.com>
2019-01-06 18:38:43 +0400

    Cовместное использование ресурсов между разными источниками/ Задача 1. Твиттер-виджет

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function loadData(url) {
+  return new Promise((done, fail) => {
+    window[showWidget] = done;
+
+    const script = document.createElement('script');
+    script.src = `${url}?callback=showWidget`;
+    document.body.appendChild(script);
+  });
+}
+
+function showWidget(data) {
+  for (const value in data) {
+    if (value === 'wallpaper' || value === 'pic') {
+      document.querySelector(`[data-${value}]`).src = data[value];
+    } else {
+      document.querySelector(`[data-${value}]`).textContent = data[value];
+    }
+  }
+}
+
+loadData('https://neto-api.herokuapp.com/twitter/jsonp');
