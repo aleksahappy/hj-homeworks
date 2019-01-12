09eb7e13ecb8b55749b4bb8e1d379e2fe9b1efa7
Aleksahappy <aleksa.happy@gmail.com>
2019-01-13 02:58:20 +0400

    Домашнее задание к лекции «Создание HTML с нуля»/ Задача 3. Страница интернет-магазина

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function createElement(node) {
+  if ((node === undefined) || (node === null) || (node === false)) {
+    return document.createTextNode('');
+  }
+  if ((typeof node === 'string') || (typeof node === 'number') || (node === true)) {
+    return document.createTextNode(node.toString());
+  }
+  if (Array.isArray(node)) {
+    return node.reduce((fragment, element) => {
+      fragment.appendChild(createElement(element));
+      return fragment;
+    }, document.createDocumentFragment());
+  }
+
+  const element = document.createElement(node.name);
+
+  if (node.props && typeof node.props === 'object') {
+    Object.keys(node.props).forEach(key => element.setAttribute(key, node.props[key]));
+  }
+
+  if (node.childs) {
+    element.appendChild(createElement(node.childs));
+  }
+
+  return element;
+}
