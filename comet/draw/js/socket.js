683149c9bab262a246197aabaa2f1bb785e11532
Aleksahappy <aleksa.happy@gmail.com>
2019-01-15 02:53:15 +0400

    Домашнее задание к лекции «Продвинутая работа с вебсокетами»/ Задача 2. Коллективное рисование

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+connection.addEventListener('open', () => {
+  editor.addEventListener('update', showCurrentState);
+});
+
+function showCurrentState(event) {
+  const canvas = event.canvas;
+  canvas.toBlob(blob => connection.send(blob));
+}
