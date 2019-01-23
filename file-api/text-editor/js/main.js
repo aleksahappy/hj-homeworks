71ce33e1c4583cdd9eb10a9d50ac5f48af8b3500
Aleksahappy <aleksa.happy@gmail.com>
2019-01-23 21:03:08 +0400

    Работа с файлами/ Задача 1.Текстовый редактор

--- file-api/text-editor/js/main.js
+++ file-api/text-editor/js/main.js
@@ -5,11 +5,13 @@ const throttle = ( handler, ms ) => {
     timeout = setTimeout( handler, ms );
   }
 };
+
 class TextEditor {
   constructor( container, storageKey = '_text-editor__content' ) {
     this.container = container;
     this.contentContainer = container.querySelector( '.text-editor__content' );
     this.hintContainer = container.querySelector( '.text-editor__hint' );
+    this.hintContent = container.querySelector( '.text-editor__hint-content' );
     this.filenameContainer = container.querySelector( '.text-editor__filename' );
     this.storageKey = storageKey;
     this.registerEvents();
@@ -18,17 +20,41 @@ class TextEditor {
   registerEvents() {
     const save = throttle( this.save.bind( this ), 1000 );
     this.contentContainer.addEventListener( 'input', save );
+
+    this.container.addEventListener('dragover', event => this.showHint(event));
+    this.container.addEventListener('dragleave', event => this.hideHint(event));
+    this.container.addEventListener('drop', event => this.loadFile(event));
   }
   loadFile( e ) {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    const txtTypeRegExp = /^text\//;
+    if (txtTypeRegExp.test(file.type)) {
+      this.readFile(file);
+      this.hideHint(e);
+    } else {
+      this.hintContent.textContent = 'Формат данного файла не поддерживается';
+    }
   }
   readFile( file ) {
+    this.contentContainer.value = '';
+    const reader = new FileReader();
+    reader.addEventListener('load', event => this.load(event.currentTarget.result));
+    reader.readAsText(file);
+    this.setFilename(file.name);
   }
   setFilename( filename ) {
     this.filenameContainer.textContent = filename;
   }
   showHint( e ) {
+    e.preventDefault();
+    this.hintContainer.classList.add('text-editor__hint_visible');
+    this.hintContent.textContent = 'Перенесите и отпустите .txt файл для загрузки содержимого в редактор';
   }
-  hideHint() {
+  hideHint( e ) {
+    if (e.type === 'drop' || e.relatedTarget === null) {
+      this.hintContainer.classList.remove('text-editor__hint_visible');
+    }
   }
   load( value ) {
     this.contentContainer.value = value || '';
